d10a5d9acef36afbe2138edddfb01d8042a55ca9
nickmullaney <nick.mullaney@gmail.com>
2023-05-18 15:54:19 -0700

    hold on to your butts

--- src/routes/cookbook.js
+++ src/routes/cookbook.js
@@ -3,16 +3,16 @@
 // Import required modules
 const express = require('express');
 const router = express.Router();
-const { recipe } = require('../models');
+const { cookbook } = require('../models');
 const validator = require('../middleware/validator');
 
 // Route to get all recipes
-router.get('/recipe', async (req, res, next) => {
+router.get('/cookbook', async (req, res, next) => {
   try {
     // Retrieve all recipes from the database
-    let recipes = await recipe.findAll();
+    let cookbooks = await cookbook.findAll();
     // Send the retrieved recipes as the response
-    res.status(200).send(recipes);
+    res.status(200).send(cookbooks);
   }
   catch (e) {
     // Pass any errors to the error handling middleware
@@ -21,15 +21,15 @@ router.get('/recipe', async (req, res, next) => {
 });
 
 // Route to get a single recipe by ID
-router.get('/recipe/:id', async (req, res, next) => {
+router.get('/cookbook/:id', async (req, res, next) => {
   // Parse the ID parameter from the request
   let id = parseInt(req.params.id);
   try {
     console.log('heres my id ', id);
     // Find a single recipe with the given ID in the database
-    let singleRecipe = await recipe.findAll({ where: { id } });
+    let singleCookbook = await cookbook.findAll({ where: { id } });
     // Send the retrieved recipe as the response
-    res.status(200).send(singleRecipe);
+    res.status(200).send(singleCookbook);
   }
   catch (e) {
     // Pass any errors to the error handling middleware
@@ -38,22 +38,22 @@ router.get('/recipe/:id', async (req, res, next) => {
 });
 
 // Route to create a new recipe
-router.post('/recipe', async (req, res, next) => {
+router.post('/cookbook', async (req, res, next) => {
   // Create a new recipe using the request body data
-  let newRecipe = await recipe.create(req.body);
+  let newCookbook = await cookbook.create(req.body);
   // Send the newly created recipe as the response
-  res.status(200).json(newRecipe);
+  res.status(200).json(newCookbook);
 });
 
 // Route to delete a recipe by ID
-router.delete('/recipe/:id', async (req, res, next) => {
+router.delete('/cookbook/:id', async (req, res, next) => {
   try {
     // Delete a recipe with the given ID from the database
-    let deleteRecipe = await recipe.findAll({ where: { id: req.params.id } });
+    let deleteCookbook = await cookbook.findAll({ where: { id: req.params.id } });
 
-    await recipe.destroy({where: {id: req.params.id}});
+    await cookbook.destroy({where: {id: req.params.id}});
     // Send the number of deleted recipes as the response
-    res.status(200).json(deleteRecipe);
+    res.status(200).json(deleteCookbook);
   }
   catch (e) {
     // Pass any errors to the error handling middleware
@@ -62,13 +62,13 @@ router.delete('/recipe/:id', async (req, res, next) => {
 });
 
 // Route to update a recipe by ID
-router.put('/recipe/:id', async (req, res, next) => {
+router.put('/cookbook/:id', async (req, res, next) => {
   try {
-    await recipe.update(req.body, { where: { id: req.params.id }});
+    await cookbook.update(req.body, { where: { id: req.params.id }});
     // Update a recipe with the given ID in the database using the request body data
-    let updatedRecipe = await recipe.update(req.body, { where: { id: req.params.id } });
+    let updatedCookbook = await cookbook.update(req.body, { where: { id: req.params.id } });
     // Send the updated recipe as the response
-    res.status(200).send(updatedRecipe);
+    res.status(200).send(updatedCookbook);
 
   } catch (e) {
     // Pass any errors to the error handling middleware
